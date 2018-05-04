36c63b8e7d744fabf33049e778e1a3cee6dc2668
fabrice <fabrice.conchou@laposte.net>
2018-05-04 18:34:50 +0200

    Commit pour tester choix personnage

--- jeu/Team.swift
+++ jeu/Team.swift
@@ -20,9 +20,28 @@ class Team {
         }
     }
     func selectCharacter() {
-        print ("Choisissez le type de personnage : a - Un combattant, b - un mage,  ")
+        print ("Choisissez le type de personnage : a - Un combattant, b - un mage, c - un nain, d - un colosse ")
+        var entJouer : String?
         var entreeJoueur = readLine()
         // tester readline si a = combattant etc...
+        _ = entreeJoueur
+        if entJouer == "a" {
+                print ("vous avez choisi un combattant")
+                
+            } else if entJouer == "b" {
+                print ("vous avez choisi un mage")
+            } else if entJouer == "c" {
+                print ("vous avez choisi un nain")
+        }
+                else if entJouer == "d" {
+                    print ("vous avez choisi un colosse")
+                }
+        
+        
+            
+         else {
+            print ("Entrer seulement a, b c ou d  !")
+}
         // chercher propriété commune à tous les instances d'une classe
         
     }
