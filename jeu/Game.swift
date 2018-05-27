515165d9c8695c8d10a3132c2f3803011b35ac99
fabrice <fabrice.conchou@laposte.net>
2018-05-27 16:40:35 +0200

    Fin étape 1 avec améliorations du game

--- jeu/Game.swift
+++ jeu/Game.swift
@@ -16,10 +16,32 @@ class Game {
     init () {
         team1 = Team ()
         team2 = Team ()
-    }
-    func createGame () {
-        print ("Bienvenue, Vous allez maintenant créer vos équipes")
-        team1.createTeam ()
+        createGame()
+        presenteTeam()
         
     }
+    func createGame () {
+        print ("Création de l'équipe 1 ...")
+        team1.createTeam ()
+        print ("Création de l'équipe 2 ...")
+        team2.createTeam()
+        
+    }
+    
+    func presenteTeam () {
+        print ("Les équipes sont créées le jeu va pouvoir commencer...")
+        print ("L'équipe 1 est composée ainsi : ")
+        for character in team1.teamMembers {
+            print ("Un personnage qui s'appelle \(character.name) qui est un \(character.typeCharac) avec \(character.lifePersonnage) de points de vie. Il a \(character.arm) comme arme et son arme inflige \(character.armPower) de points de vie")
+            
+        }
+        
+        
+        print ("L'équipe 2 est composée ainsi : ")
+        for character in team2.teamMembers {
+            print ("Un personnage qui s'appelle \(character.name) qui est un \(character.typeCharac) avec \(character.lifePersonnage) de points de vie. Il a \(character.arm) comme arme et son arme inflige \(character.armPower) de points de vie")
+            
+        }
+    }
 }
+
--- jeu/main.swift
+++ jeu/main.swift
@@ -7,30 +7,6 @@
 //
 
 import Foundation
-
-print("Hello, World!")
-
-var team1 = Team()
-print ("Création de l'équipe 1 ...")
-team1.createTeam()
+Game()
 
 
-
-
-var team2 = Team()
-print ("Création de l'équipe 2 ...")
-team2.createTeam()
-
-print ("Les équipes sont créées le jeu va pouvoir commencer...")
-print ("L'équipe 1 est composée ainsi : ")
-for character in team1.teamMembers {
-    print ("Un personnage qui s'appelle \(character.name) qui est un \(character.typeCharac) avec \(character.lifePersonnage) de points de vie. Il a \(character.arm) comme arme et son arme inflige \(character.armPower) de points de vie")
-    
-}
-
-
-print ("L'équipe 2 est composée ainsi : ")
-for character in team2.teamMembers {
-    print ("Un personnage qui s'appelle \(character.name) qui est un \(character.typeCharac) avec \(character.lifePersonnage) de points de vie. Il a \(character.arm) comme arme et son arme inflige \(character.armPower) de points de vie")
-    
-}
